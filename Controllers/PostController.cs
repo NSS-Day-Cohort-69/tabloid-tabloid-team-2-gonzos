ba27ea963bf9e0569e6a4073017d0ca55f34bb73
Kayce-Bracewell <Kayceb98@gmail.com>
2024-05-29 11:03:57 -0500

    all posts view is completed with filtering done

---
+++ Controllers/PostController.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Mvc;
+using Tabloid.Models;
+using Tabloid.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Tabloid.Models.DTOs;
+
+namespace Tabloid.Controllers;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class PostController : ControllerBase
+{
+    private TabloidDbContext _dbContext;
+
+    public PostController(TabloidDbContext context)
+    {
+        _dbContext = context;
+    }
+
+    [HttpGet]
+    // [Authorize]
+    public IActionResult Get()
+    {
+        List<Post> posts = _dbContext.Posts.Include(p => p.Author).ToList();
+
+        List<PostDTO> postDTOs = posts.Select(p => new PostDTO
+        {
+            Id = p.Id,
+            Title = p.Title,
+            AuthorId = p.AuthorId,
+            Author = new UserProfileDTO
+            {
+                Id = p.Author.Id,
+                FirstName = p.Author.FirstName,
+                LastName = p.Author.LastName,
+                Username = p.Author.UserName,
+                Email = p.Author.Email,
+                CreateDateTime = p.Author.CreateDateTime,
+                ImageLocation = p.Author.ImageLocation,
+                IdentityUserId = p.Author.IdentityUserId,
+                IsActive = p.Author.IsActive
+            },
+            PublicationDate = p.PublicationDate,
+            Body = p.Body,
+            CategoryId = p.CategoryId,
+            HeaderImage = p.HeaderImage,
+            PostApproved = p.PostApproved,
+            EstimatedReadTime = p.EstimatedReadTime
+        }).ToList();
+
+        return Ok(postDTOs);
+    }
+
+}
--- Migrations/20240529143615_newCreate.Designer.cs
+++ Migrations/20240529143615_newCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Tabloid.Data;
@@ -12,9 +13,11 @@ using Tabloid.Data;
 namespace Tabloid.Migrations
 {
     [DbContext(typeof(TabloidDbContext))]
-    partial class TabloidDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240529143615_newCreate")]
+    partial class newCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
