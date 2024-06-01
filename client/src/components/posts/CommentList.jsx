f32add48e4443c7b81f05382ec5fd641abffd3ca
Ezra Brewer <ezjbrewer@gmail.com>
2024-06-01 14:36:11 -0500

    Refactored a post's comments onto separate component

--- client/src/components/posts/CommentList.jsx
+++ client/src/components/posts/CommentList.jsx
@@ -1,7 +1,79 @@
-export const CommentList = () => {
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
+import { deleteComment, getComments } from "../../managers/commentManager.js";
+import { Button } from "reactstrap";
+import { useNavigate } from "react-router-dom";
+import shrug from "../images/shrug.png"
+
+export const CommentList = ({loggedInUser}) => {
+    const {id} = useParams();
+    const navigate = useNavigate();
+    const [comments, setComments] = useState([]);
+    const [deleteConfirmWindow, toggleDeleteConfirmWindow] = useState(0);
+
+    useEffect(() => {
+        getComments(id).then(setComments)
+    }, [id]);
+
+    const handleCommentDeletion = (commentId) => {
+        deleteComment(commentId).then(() => {
+            getComments(id).then(setComments)
+        toggleDeleteConfirmWindow(0)
+        })
+    };
+    
     return(
-        <div>
-            Do later!
+        <div className="comments-container">
+            {comments.length < 1 ?
+            <div className="comments-container-empty">
+                No comments made yet...
+                <img src={shrug} alt="shrug" width="200" />
+            </div> : comments.map(c => {
+                        return(
+                            <div className="comment-container" key={c.id}>
+                                <p><b>{c.author.firstName} {c.author.lastName}</b> • {c.creationDate ? new Date(c.creationDate).toLocaleDateString('en-US') : 'N/A'}</p>
+                                <p>Subject: {c.subject}</p>
+                                <p>{c.content}</p>
+                                {loggedInUser?.id == c.authorId ? 
+                                <div>
+                                    <Button
+                                        className="comment-option"
+                                        color="danger"
+                                        onClick={() => toggleDeleteConfirmWindow(c.id)}>
+                                        🗑️
+                                    </Button>
+                                    <Button
+                                        className="comment-option"
+                                        color="success"
+                                        onClick={() => navigate(`/posts/editComment/${c.id}`)}>
+                                        ✏️
+                                    </Button>
+                                </div>
+                                : <div></div>}
+                                {deleteConfirmWindow == c.id ?
+                                <div>
+                                    <p>Are you sure you want to delete this comment?</p>
+                                    <Button
+                                        className="comment-option"
+                                        color="danger"
+                                        onClick={() => toggleDeleteConfirmWindow(0)}>
+                                        No
+                                    </Button>
+                                    <Button
+                                        className="comment-option"
+                                        color="success"
+                                        onClick={() => handleCommentDeletion(c.id)}>
+                                        Yes
+                                    </Button>
+                                </div>
+                                :<div></div>}
+                            </div>
+                        )
+                    })}
+                <Button className="back-btn" onClick={() => {
+                    navigate(`/posts/${id}`)}}>
+                    Back
+                </Button>
         </div>
     )
 }
--- client/src/components/posts/PostDetails.jsx
+++ client/src/components/posts/PostDetails.jsx
@@ -9,9 +9,7 @@ export const PostDetails = ({ loggedInUser }) => {
     const [post, setPost] = useState({});
     const [showConfirmation, setShowConfirmation] = useState(false);
     const { id } = useParams();
-    const [comments, setComments] = useState([]);
     const [addCommentSwitch, toggleAddComment] = useState(false);
-    const [deleteConfirmWindow, toggleDeleteConfirmWindow] = useState(0);
     const [commentObj, setCommentObj] = useState(
     {
         Subject: "",
@@ -37,23 +35,12 @@ export const PostDetails = ({ loggedInUser }) => {
             });
     };
 
-    useEffect(() => {
-        getComments(id).then(setComments)
-    }, [post]);
-
     const handleInputChange = (e) => {
         const { name, value } = e.target
         setCommentObj(prevState => ({
             ...prevState,
             [name]: value
         }))
-    };
-
-    const handleCommentDeletion = (id) => {
-        deleteComment(id).then(() => {
-            getComments(post.id).then(setComments)
-        toggleDeleteConfirmWindow(0)
-        })
     };
 
     const resetCommentFields = () => {
@@ -63,20 +50,30 @@ export const PostDetails = ({ loggedInUser }) => {
 
     return (
         <>
-            <h2>Here are your details...</h2>
-            <div className="post-main-container">
-                <h3>Title: {post.title}</h3>
-                <h4>Body: {post.body}</h4>
-                {post.headerImage && <img className="post-image-header" src={post.headerImage} alt={`${post.title} header`} />}
-                <h6>
-                    Publication Date:{" "}
-                    {post.publicationDate ? new Date(post.publicationDate).toLocaleDateString("en-US") : "N/A"}
-                </h6>
-                <p>Username: {post.author?.identityUser?.userName}</p>
-                <div className="comments-container">
-                    <h3>Comments</h3>
-                    <Button onClick={() => toggleAddComment(!addCommentSwitch)}>{addCommentSwitch ? "Back" : "Add a comment"}</Button>
-                    {addCommentSwitch ?
+            <div className="post-container">
+                <h2>Here are your details...</h2>
+                <div className="post-main-container">
+                    <h3>Title: {post.title}</h3>
+                    <h4>Body: {post.body}</h4>
+                    {post.headerImage && <img className="post-image-header" src={post.headerImage} alt={`${post.title} header`} />}
+                    <h6>
+                        Publication Date:{" "}
+                        {post.publicationDate ? new Date(post.publicationDate).toLocaleDateString("en-US") : "N/A"}
+                    </h6>
+                    <p>Username: {post.author?.identityUser?.userName}</p>
+            </div>
+            <div className="comments-container">
+                <h3>Comments</h3>
+                <Button
+                    color="info"
+                    onClick={() => navigate(`/comments/post/${id}`)}>
+                    See Comments
+                </Button>
+                <Button
+                    color="success"
+                    onClick={() => toggleAddComment(!addCommentSwitch)}>{addCommentSwitch ? "Back" : "Add a comment"}
+                </Button>
+                {addCommentSwitch ?
                     <div>
                         <Label>
                             Subject
@@ -96,59 +93,32 @@ export const PostDetails = ({ loggedInUser }) => {
                             Post Comment
                         </Button>
                     </div> : <div></div>}
-                    {comments.map(c => {
-                        return(
-                            <div className="comment-container" key={c.id}>
-                                <p><b>{c.author.firstName} {c.author.lastName}</b> • {c.creationDate ? new Date(c.creationDate).toLocaleDateString('en-US') : 'N/A'}</p>
-                                <p>Subject: {c.subject}</p>
-                                <p>{c.content}</p>
-                                {loggedInUser?.id == c.authorId ? 
-                                <div>
-                                    <Button
-                                        className="comment-option"
-                                        color="danger"
-                                        onClick={() => toggleDeleteConfirmWindow(c.id)}>
-                                        🗑️
-                                    </Button>
-                                    <Button
-                                        className="comment-option"
-                                        color="success"
-                                        onClick={() => navigate(`/posts/editComment/${c.id}`)}>
-                                        ✏️
-                                    </Button>
-                                </div>
-                                : <div></div>}
-                                {deleteConfirmWindow == c.id ?
-                                <div>
-                                    <p>Are you sure you want to delete this comment?</p>
-                                    <Button
-                                        className="comment-option"
-                                        color="danger"
-                                        onClick={() => toggleDeleteConfirmWindow(0)}>
-                                        No
-                                    </Button>
-                                    <Button
-                                        className="comment-option"
-                                        color="success"
-                                        onClick={() => handleCommentDeletion(c.id)}>
-                                        Yes
-                                    </Button>
-                                </div>
-                                :<div></div>}
-                            </div>
-                        )
-                    })}
                 </div>
                 {loggedInUser && post.authorId === loggedInUser.id && (
                     <>
-                        <button onClick={() => setShowConfirmation(true)}>Delete</button>
-                        {showConfirmation && (
-                            <div>
-                                <p>Are you sure you want to delete this post?</p>
-                                <button onClick={handleDelete}>Yes</button>
-                                <button onClick={() => setShowConfirmation(false)}>No</button>
-                            </div>
-                        )}
+                        <div className="options-container">
+                            <h3>Options</h3>
+                            <Button
+                                color="danger"
+                                onClick={() => setShowConfirmation(true)}>
+                                    Delete
+                            </Button>
+                            {showConfirmation && (
+                                <div>
+                                    <p>Are you sure you want to delete this post?</p>
+                                    <Button
+                                        color="success"
+                                        onClick={handleDelete}>
+                                            Yes
+                                    </Button>
+                                    <Button
+                                        color="danger"
+                                        onClick={() => setShowConfirmation(false)}>
+                                            No
+                                    </Button>
+                                </div>
+                            )}
+                        </div>
                     </>
                 )}
             </div>
