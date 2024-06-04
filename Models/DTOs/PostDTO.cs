0566c387719ba7dc0a02fa7622ab93ceb48f1d7c
Zavier Hopson <zo.hopson94@gmail.com>
2024-06-04 11:53:41 -0500

    updated categoryid property

--- Models/DTOs/PostDTO.cs
+++ Models/DTOs/PostDTO.cs
@@ -5,15 +5,15 @@ namespace Tabloid.Models.DTOs
         public int Id { get; set; }
         public string Title { get; set; }
         public int AuthorId { get; set; }
-        public UserProfileDTO Author {get; set;}
+        public UserProfileDTO Author { get; set; }
         public DateTime PublicationDate { get; set; }
         public string Body { get; set; }
-        public int CategoryId { get; set; }
+        public int? CategoryId { get; set; }
         public CategoryDTO Category { get; set; }
         public string HeaderImage { get; set; }
         public bool PostApproved { get; set; }
         public int? EstimatedReadTime { get; set; }
 
-        public List<TagDTO> Tags {get; set;} 
+        public List<TagDTO> Tags { get; set; }
     }
 }
