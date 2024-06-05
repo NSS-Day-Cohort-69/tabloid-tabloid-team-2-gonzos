74ae9821361366e8ce290f5b5ad66df2a9899ada
Zavier Hopson <zo.hopson94@gmail.com>
2024-06-05 10:17:37 -0500

    added delete logic

--- client/src/components/categories/CategoryList.jsx
+++ client/src/components/categories/CategoryList.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getAllCategories } from "../../managers/categoryManager";
-import { EditCategory } from "./EditCategory";
+import ConfirmDelete from "../modals/ConfirmDelete";
+import { deleteCategory } from "../../managers/categoryManager";
 
 export default function CategoryList() {
   const [categories, setCategories] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [deleteCategoryById, setDeleteCategoryById] = useState(null);
 
   const getCategories = () => {
     getAllCategories().then((data) => {
@@ -18,6 +21,21 @@ export default function CategoryList() {
     getCategories();
   }, []);
 
+  const toggleModal = () => {
+    setModalOpen(!modalOpen);
+  };
+
+  const handleDeleteModal = (id) => {
+    setDeleteCategoryById(id);
+    toggleModal();
+  };
+
+  const handleDeleteCategory = () => {
+    deleteCategory(deleteCategoryById).then(() => {
+      setCategories(categories.filter((c) => c.Id !== deleteCategoryById));
+      toggleModal();
+    });
+  };
   return (
     <>
       <p>Category List</p>
@@ -28,12 +46,24 @@ export default function CategoryList() {
             <button>
               <Link to={`/editcategory/${c.id}`}>Edit Category</Link>
             </button>
+            <button onClick={() => handleDeleteModal(c.id)}>
+              Delete Category
+            </button>
           </p>
         ))}
       </div>
       <button>
         <Link to={"/newcategory"}>New Category</Link>
       </button>
+      <ConfirmDelete
+        isOpen={modalOpen}
+        toggle={toggleModal}
+        confirmDelete={handleDeleteCategory}
+        typeName={"category"}
+      />
     </>
   );
 }
+
+//button after edit, import delete code, have function within exports that handles delete using imported function, deletes by id
+//modals for pop up to confirm deletion in reactstrap check team-one-more-div
